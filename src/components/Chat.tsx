6799311a8b1da7f7b8536840e67cf10716962cb4
juanarancibia <arancibia.juan22@gmail.com>
2023-03-20 20:41:53 -0300

    validation of ongoing request, overflow-x hidden

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -9,8 +9,11 @@ export interface ChatProps {}
 const Chat: React.FC<ChatProps> = () => {
   const [inputText, setInputText] = useState("");
   const [chatGPTResponse, setChatGPTResponse] = useState("");
+  const [isChatStreaming, setIsChatStreaming] = useState(false);
 
   const submitInput = async () => {
+    setIsChatStreaming(true);
+
     const response = await fetch("/api/chat", {
       method: "POST",
       headers: {
@@ -25,11 +28,13 @@ const Chat: React.FC<ChatProps> = () => {
     console.log("Edge function returned");
 
     if (!response.ok) {
+      setIsChatStreaming(false);
       throw new Error(response.statusText);
     }
 
     const data = response.body;
     if (!data) {
+      setIsChatStreaming(false);
       return;
     }
 
@@ -46,6 +51,8 @@ const Chat: React.FC<ChatProps> = () => {
 
       setChatGPTResponse((prevValue) => prevValue + chunkValue);
     }
+
+    setIsChatStreaming(false);
   };
 
   return (
@@ -60,14 +67,16 @@ const Chat: React.FC<ChatProps> = () => {
       >
         <CardContent className="flex h-full  flex-col">
           <div className="border-1 mb-4 h-[83%] flex-1 border border-[#262339]">
-            <p className="m-4 h-[95%] overflow-scroll whitespace-pre-wrap text-[#c7c7c7]">
+            <p className="m-4 h-[95%] overflow-scroll  overflow-x-hidden whitespace-pre-wrap text-[#c7c7c7]">
               {chatGPTResponse}
             </p>
           </div>
           <ChatInput
             inputText={inputText}
             setInputText={setInputText}
-            onEnter={submitInput}
+            onEnter={() => {
+              console.log(inputText);
+            }}
           />
           <Button
             fullWidth
@@ -75,6 +84,7 @@ const Chat: React.FC<ChatProps> = () => {
             color="secondary"
             className="mt-2 p-2"
             onClick={submitInput}
+            disabled={isChatStreaming}
           >
             Aceptar
           </Button>
